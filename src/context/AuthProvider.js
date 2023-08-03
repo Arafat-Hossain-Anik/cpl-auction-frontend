2e62e0ef4edd405b6743c63f94d06491a9333c3b
Arafat-Hossain-Anik <arafatskd@gmail.com>
2023-08-04 01:21:45 +0600

    usecart removed

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,19 +1,20 @@
 import React, { createContext } from 'react';
-import UseCart from '../hooks/UseCart';
+// import UseCart from '../hooks/UseCart';
 import useFirebase from '../hooks/useFirebase';
 
 export const AuthContext = createContext();
 
-const AuthProvider = ({children} ) => {
+const AuthProvider = ({ children }) => {
     // const {children} = props;
     const allContext = useFirebase();
     // hooks 
-    const {addToCart,selectedBooking, remove} = UseCart();
+    // const {addToCart,selectedBooking, remove} = UseCart();
     const data = {
-        contexts:allContext,
-         addToCart,
-         selectedBooking,
-        remove}
+        contexts: allContext,
+        //  addToCart,
+        //  selectedBooking,
+        // remove
+    }
     return (
         <div>
             <AuthContext.Provider value={data}>
